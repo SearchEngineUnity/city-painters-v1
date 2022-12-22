520771bef0b0234cd6cdbcf3278585fd8a1dcc35
sc-searchengineunity <sharon@searchengineunity.com>
2022-12-22 12:39:02 -0500

    check if the url change from /~partytown to /partytown

--- web/gatsby-ssr.js
+++ web/gatsby-ssr.js
@@ -9,32 +9,16 @@ export const onRenderBody = ({ setHeadComponents, setPreBodyComponents }) => {
       key="partytown"
       debug
       forward={['dataLayer.push']}
-      // resolveUrl={(url) => {
-      //   // https://partytown.builder.io/proxying-requests
-      //   const proxyDomains = ['www.googletagmanager.com'];
-      //   if (proxyDomains.includes(url.hostname)) {
-      //     const proxyUrl = new URL('https://wwww.thecitypanters.com/~partytown');
-      //     proxyUrl.searchParams.append('url', url);
-      //     return proxyUrl;
-      //   }
-      // }}
-    />,
-    <script
-      type="text/partytown"
-      key="gtm-script"
-      dangerouslySetInnerHTML={{
-        __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-    })(window,document,'script','dataLayer','GTM-P68M4K4');`,
+      resolveUrl={(url) => {
+        // https://partytown.builder.io/proxying-requests
+        const proxyDomains = ['www.googletagmanager.com'];
+        if (proxyDomains.includes(url.hostname)) {
+          const proxyUrl = new URL('https://wwww.thecitypanters.com/partytown');
+          proxyUrl.searchParams.append('url', url);
+          return proxyUrl;
+        }
       }}
     />,
-    // <script
-    //   key="analytics"
-    //   src="https://www.google-analytics.com/analytics.js"
-    //   type="text/partytown"
-    // />,
   ]);
 
   // For GTM, we will need to add this noscript tag to the body of the HTML
